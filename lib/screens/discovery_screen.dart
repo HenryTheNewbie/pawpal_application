331e7fdfc8334c6bcc60520bf284810e731d09c5
Harishinee K.Vesupathy <107970624+hashdonut@users.noreply.github.com>
2025-05-26 12:50:22 +0800

    Revert "Commit #7"

--- lib/screens/discovery_screen.dart
+++ lib/screens/discovery_screen.dart
@@ -8,13 +8,70 @@ import '../routes/routes.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/cupertino.dart';
 
-class DiscoveryScreen extends StatelessWidget {
+class DiscoveryScreen extends StatefulWidget {
   const DiscoveryScreen({super.key});
+
+  @override
+  State<DiscoveryScreen> createState() => _DiscoveryScreenState();
+}
+
+class _DiscoveryScreenState extends State<DiscoveryScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    _DiscoveryHome(),
+    // ChatScreen(),
+    // ProfileScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      backgroundColor: theme.colorScheme.background,
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: SizedBox(
+        height: 60,
+        child: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          backgroundColor: Colors.white,
+          selectedItemColor: Colors.black,
+          unselectedItemColor: Colors.grey,
+          showUnselectedLabels: false,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.paw),
+              label: 'Discover',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.chat_bubble_2),
+              label: 'Chat',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.person_crop_circle),
+              label: 'Profile',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _DiscoveryHome extends StatelessWidget {
+  const _DiscoveryHome();
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
---
+++ lib/screens/user_profile.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import '../firebase_options.dart';
+import '../theme/theme.dart';
+import '../routes/routes.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+// UserProfileScreen
+// This screen displays the user's profile information and allows them to edit their profile.
